ceb25dd07a642994092549f600893fc9f4dda7e4
sominsomin <Simon.Zoller@stepstone.com>
2022-07-05 11:48:29 +0200

    more refac

--- mrt_phase_pde/pde/own_method/extract_curves_from_T_N/plot_curves_from_T_N.py
+++ mrt_phase_pde/pde/own_method/extract_curves_from_T_N/plot_curves_from_T_N.py
@@ -20,7 +20,7 @@ delta_a = equation_config['delta_a']
 limit_cycle_file_path = filepaths['limit_cycle_path']
 limit_cycle = read_curve_from_file(limit_cycle_file_path)
 
-D = 1.0
+D = 0.1
 
 
 def find_nearest(array, value):
@@ -36,18 +36,19 @@ if __name__ == '__main__':
 
     T_N = T_N_class.load(f'..\\result\\T_N_6_D_{D}.pickle')
 
-    curves = get_curve_for_T_N(T_N, D, type='deterministic')
+    curves = get_curve_for_T_N(T_N, D)
 
     __x, __y = np.meshgrid(T_N.v, T_N.a)
 
     plt.figure()
     for i, curve in enumerate(curves):
+        step_size = int(len(curve) / 3)
+        curve[:, 1] = savgol_filter(curve[:, 1], step_size, np.min([step_size, 5]) - 1)
         if i == 0:
             plt.plot(curve[:, 0], curve[:, 1], 'b-', label='isochrones')
         else:
             plt.plot(curve[:, 0], curve[:, 1], 'b-')
-        # curve[:, 1] = savgol_filter(curve[:, 1], int(len(curve)/5), 4)
-        plt.plot(curve[:, 0], curve[:, 1], 'b-')
+        # plt.plot(curve[:, 0], curve[:, 1], 'b-')
         # plt.text(1, curve[-1, 1], '')
 
     plt.xlabel('v')
@@ -59,16 +60,17 @@ if __name__ == '__main__':
     plt.legend()
 
     # plt.savefig(f'img\\isochrones_for_D_{D}_comparison_numeric_isochrones.png')
+    # plt.savefig(f'img\\isochrones_for_D_{D}.png')
 
     det_file_paths = '..\\..\\..\\..\\mrt_phase_numeric\\data\\results\\isochrones\\from_timeseries_grid\\deterministic\\D_0.0'
     stochastic = f'..\\..\\..\\..\\mrt_phase_numeric\\data\\results\\isochrones\\from_timeseries\\stochastic\\D_{D}'
 
-    if D == 0.0:
-        isochrones = load_isochrones(det_file_paths)
-    else:
-        isochrones = load_isochrones(stochastic)
-
-    plot_isochrones(isochrones, plt, 'g--')
+    # if D == 0.0:
+    #     isochrones = load_isochrones(det_file_paths)
+    # else:
+    #     isochrones = load_isochrones(stochastic)
+    #
+    # plot_isochrones(isochrones, plt, 'g--')
 
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = OrderedDict(zip(labels, handles))
