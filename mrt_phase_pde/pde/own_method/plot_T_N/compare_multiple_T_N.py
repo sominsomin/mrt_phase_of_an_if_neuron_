e8a9aba60d05ce5e902f6ad3e32d429d82f03f17
sominsomin <Simon.Zoller@stepstone.com>
2022-07-05 13:02:34 +0200

    more convenience functions

--- mrt_phase_pde/pde/own_method/plot_T_N/compare_multiple_T_N.py
+++ mrt_phase_pde/pde/own_method/plot_T_N/compare_multiple_T_N.py
@@ -4,41 +4,57 @@ import numpy as np
 from mrt_phase_pde.pde.own_method.src.T_0.T_0 import T_0
 from mrt_phase_pde.pde.own_method.src.T_0.T_N import T_N
 
-D = .1
-
 n = list(range(1, 6))
-diff_mean = []
-diff_max = []
-diff_min = []
 
-for i in n:
-    n_1 = i
-    n_2 = i-1
 
-    T_1 = f'..\\result\\T_N_{n_1}_D_{D}.pickle'
-    T_2 = f'..\\result\\T_N_{n_2}_D_{D}.pickle'
+def get_diff(D):
+    diff_mean = []
+    diff_max = []
+    diff_min = []
 
-    t_1 = T_N.load(T_1)
-    t_2 = T_0.load(T_2)
+    for i in n:
+        n_1 = i
+        n_2 = i-1
 
-    max_a = 3
-    max_n_a = np.where(t_1.a == max_a)[0][0] + 1
+        T_1 = f'..\\result\\T_N_{n_1}_D_{D}.pickle'
+        T_2 = f'..\\result\\T_N_{n_2}_D_{D}.pickle'
 
-    diff = t_1.T[:, :max_n_a] - t_2.T[:, :max_n_a]
+        t_1 = T_N.load(T_1)
+        t_2 = T_0.load(T_2)
 
-    diff_mean.append(np.mean(diff))
-    diff_max.append(np.max(diff))
-    diff_min.append(np.min(diff))
+        max_a = 3
+        max_n_a = np.where(t_1.a == max_a)[0][0] + 1
 
-plt.plot(n, diff_mean, label='mean($T_N(v,a) - T_{N-1}(v,a)$)')
-plt.plot(n, diff_max, label='max($T_N(v,a) - T_{N-1}(v,a)$)')
-plt.plot(n, diff_min, label='min($T_N(v,a) - T_{N-1}(v,a)$)')
-plt.xlabel('N')
-plt.ylabel('$T_N(v,a) - T_{{N-1}}(v,a)$')
-plt.title(f'$D = {D}$')
-plt.legend()
+        diff = t_1.T[:, :max_n_a] - t_2.T[:, :max_n_a]
 
-plt.savefig(f'..\\img\\T_N_diff_over_N_D_{D}.png')
-# plt.show()
+        diff_mean.append(np.mean(diff))
+        diff_max.append(np.max(diff))
+        diff_min.append(np.min(diff))
 
-print(f'D = {D}: {diff_mean[-1]}')
+    return diff_mean, diff_min, diff_max
+
+
+def plot_T_N_over_time(D):
+    diff_mean, diff_min, diff_max = get_diff(D)
+
+    plt.figure()
+    plt.plot(n, diff_mean, label='mean($T_N(v,a) - T_{N-1}(v,a)$)')
+    plt.plot(n, diff_max, label='max($T_N(v,a) - T_{N-1}(v,a)$)')
+    plt.plot(n, diff_min, label='min($T_N(v,a) - T_{N-1}(v,a)$)')
+    plt.xlabel('N')
+    plt.ylabel('$T_N(v,a) - T_{{N-1}}(v,a)$')
+    plt.title(f'$D = {D}$')
+    plt.legend()
+
+    plt.savefig(f'..\\img\\T_N_diff_over_N_D_{D}.png')
+    # plt.show()
+
+    print(f'D = {D}: {diff_mean[-1]}')
+
+
+D_list = [0.0, 0.1, 0.25, 0.5, 1.0]
+
+
+if __name__=='__main__':
+    for D in D_list:
+        plot_T_N_over_time(D)
